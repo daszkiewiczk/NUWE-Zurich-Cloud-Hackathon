e8361f50d5b79602869a58f44adc6e9f875a23d7
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-06-20 21:40:59 +0200

    variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,64 @@
+variable "lambda_name" {
+    type = string
+    default = "update_clients_table"
+}
+
 variable "runtime" {
     type = string
     default = "3.10"
 }
 
-variable "lambda_name" {
+
+
+
+variable "bucket_name" {
     type = string
-    default = "update_clients_table"
+    default = "daszkiewiczk-clients"
 }
+
+variable "trigger_file" {
+    type = string
+    default = "client_data.json"
+}
+
+
+
+
+
+variable "dynamodb_table_name" {
+    type = string
+    default = "daszkiewiczk-clients"
+}
+
+variable "dynamodb_table_hash_key" {
+    type = string
+    default = "id"
+}
+
+variable "dynamodb_table_range_key" {
+    type = string
+    default = "plate"
+}
+
+variable "dynamodb_table_read_capacity" {
+    type = number
+    default = 1
+}
+ 
+variable "dynamodb_table_write_capacity" {
+    type = number
+    default = 1
+}
+  
+
+
+variable "stage" {
+    type    = string
+    default = "local"
+}
+
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+ 
